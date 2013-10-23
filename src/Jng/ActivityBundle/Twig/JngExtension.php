6513e4d5ec0a8d9ab2a8c2c7eaa1806c6f9b9602
julien <jnegre@seriel.net>
2013-10-23 22:50:40 +0200

    edit customer filter date

--- src/Jng/ActivityBundle/Twig/JngExtension.php
+++ src/Jng/ActivityBundle/Twig/JngExtension.php
@@ -12,15 +12,27 @@ class JngExtension extends \Twig_Extension
         );
     }
     
-    public function diffToDateFilter($date1, $date2, $unit )
+    public function diffToDateFilter($date1, $date2, $unit=null )
     {
+        if( is_null($date2) )
+            return;
+        
         $t = $date2->getTimestamp()-$date1->getTimestamp();
+        
+        if( is_null($unit) ){
+            $unit="s";
+            if( $t >= 3600)
+                $unit = "h";
+            else if( $t >= 60)
+                $unit = "min";
+        }
+        
         if( $unit == "s" )
             return $t." s";
         else if( $unit == "min" )
-            return ($t*60)." min";
+            return round($t/60)." min";
         else if( $unit == "h" )
-            return ($t*60*60)." h";
+            return round($t/3600)." h";
         
     }
     
